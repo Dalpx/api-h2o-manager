b0e353a346458bf13916a516decbce5ee33695d9
Dalpx <dalpx2003@gmail.com>
2026-03-22 16:55:05 -0400

    Rutas añadidas (otra vez)

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\V1\ClienteController;
 use App\Http\Controllers\Api\V1\DocumentoController;
 use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\V1\SucursalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,14 +11,16 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function(){
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
     //Route::get('logout', [AuthController::class, 'logout']);
     //Route::apiResource('customers', CustomerController::class);
     Route::apiResource('cliente', ClienteController::class);
-    
+
     Route::post('documentoFiscal/lote', [DocumentoController::class, 'bulkStore']);
     Route::apiResource('documentoFiscal', DocumentoController::class);
-    
+
     Route::apiResource('usuario', UserController::class);
+
+    Route::apiResource('sucursal', SucursalController::class);
     //Route::post('invoices/bulk', ['uses' => 'InvoiceController@bulkStore']);
 });
